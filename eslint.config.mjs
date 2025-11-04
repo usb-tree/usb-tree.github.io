8deacb9b73b8037407659cd3689620cb40fd37f1
AOzmond <alastair.ozmond@gmail.com>
2025-11-04 03:18:05 -0800

    Update dependencies and improve ESLint configuration
    - Updated package dependencies to their latest versions in `package.json`.
    - Switched the `lint` script to use `eslint .` instead of `next lint`.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import next from "eslint-config-next";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  next,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
@@ -13,6 +11,15 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
 ]);
 
 export default eslintConfig;
