08b128acf283b75b3d505905dd1d18927710c52d
AOzmond <alastair.ozmond@gmail.com>
2025-11-10 02:46:11 -0800

    made multi-os background transparent and removed css background color.

--- components/Features/SingleFeature.tsx
+++ components/Features/SingleFeature.tsx
@@ -25,7 +25,7 @@ export default function SingleFeature({ feature }: { feature: Feature }) {
         viewport={{ once: true }}
         className="shadow-solid-3 hover:shadow-solid-4 dark:border-strokedark dark:bg-blacksection dark:hover:bg-hoverdark z-40 rounded-lg border border-white bg-white p-7.5 transition-all xl:p-12.5"
       >
-        <div className="bg-primary relative flex items-center justify-center rounded-[4px]">
+        <div className="relative flex items-center justify-center rounded-[4px]">
           <img src={icon} alt={alt} />
         </div>
         <h3 className="xl:text-itemtitle mt-7.5 mb-5 text-xl font-semibold text-black dark:text-white">
